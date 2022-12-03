46787d00571055c8aea9eaa8cb5d62034b116d44
Nexusrex <ahmedtahoun55@gmail.com>
2022-12-03 21:52:08 +0200

    Added vaccine table to the user panel, fixing some names.

--- View/user_panel.php
+++ View/user_panel.php
@@ -11,38 +11,55 @@
     <div class="col-md-12">
         <div class="card_header">
             <h1>Welcome ,User name</h1>
-            <p>available vaccination center » 2</p>
+            <p>Vaccine doses » 0</p>
         </div>
     </div>
 </div>
 
+<div class="row">
+    <div class="col">
+        <div class="card">
+            <div class="card-body">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col">Gap between doses</th>
+                            <th scope="col">Precautions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
 
 <div class="row">
     <div class="col">
         <div class="card">
             <div class="card-body">
                 <form action="" method="post">
-
                     <div class="row">
                         <div class="col">
-                            <p>Center</p>
+                            <p>Vaccination Center</p>
                             <p><label for="res_center"></label>
-                                <select name="reserve_center" id="res_center">
-                                    <option>kafr nsar</option>
-                                </select>
+                            <select name="reserve_center" id="res_center">
+                                <option>kafr nsar</option>
+                            </select>
                             </p>
                         </div>
                         <div class="col">
-                            <p>Reserve vaccine </p>
-                                <label for="res_vaccine"></label>
-                                <select name="reserve_vaccine" id="res_vaccine">
-                                    <option>corona 69</option>
-                                </select>
-
-
+                            <p>Vaccine</p>
+                            <label for="res_vaccine"></label>
+                            <select name="reserve_vaccine" id="res_vaccine">
+                                <option>corona 69</option>
+                            </select>
                         </div>
                         <div class="col">
-                            <p>Birth date
+                            <p>Dose date</p>
                             <input type="date" name="reserve_date" id="birthday"></p>
                         </div>
                     </div>
