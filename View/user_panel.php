8da91689d0d2ece8afb9ab444e892b7147e5ee85
AhmadNader319 <anader3194747@gmail.com>
2022-12-05 22:53:20 +0200

    update 4 pages (admin_panel&find_vaccination_center&user_panel&vaccination_center_panel) + update css completely

--- View/user_panel.php
+++ View/user_panel.php
@@ -2,74 +2,58 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Sign-Up</title>
+    <title>user_panel</title>
     <link href="Css/bootstrap.min.css" rel="stylesheet">
     <link href="Css/Style_sheet.css" rel="stylesheet">
 </head>
 <body>
-<div class="row">
-    <div class="col-md-12">
-        <div class="card_header">
-            <h1>Welcome ,User name</h1>
-            <p>Vaccine doses » 0</p>
-        </div>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col">
-        <div class="card">
-            <div class="card-body">
-                <table class="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Gap between doses</th>
-                            <th scope="col">Precautions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                    </tbody>
-                </table>
+    <div class="row">
+        <div class="col-12">
+            <div class="cardhead">
+                <h>Welcome</h>
+                <p>Available vaccination center</p>
             </div>
         </div>
     </div>
-</div>
 
-<div class="row">
-    <div class="col">
-        <div class="card">
-            <div class="card-body">
+
+    <div class="row">
+        <div class="col">
+            <div class="cardbody">
                 <form action="" method="post">
                     <div class="row">
+                        <div class="col-1"></div>
                         <div class="col">
-                            <p>Vaccination Center</p>
+                            <p>Center</p>
                             <p><label for="res_center"></label>
-                            <select name="reserve_center" id="res_center">
-                                <option>kafr nsar</option>
-                            </select>
+                                <select name="reserve_center" id="res_center">
+                                    <option>kafr nsar</option>
+                                </select>
                             </p>
                         </div>
                         <div class="col">
-                            <p>Vaccine</p>
+                            <p>Reserve vaccine </p>
                             <label for="res_vaccine"></label>
                             <select name="reserve_vaccine" id="res_vaccine">
                                 <option>corona 69</option>
                             </select>
+
+
                         </div>
                         <div class="col">
-                            <p>Dose date</p>
-                            <input type="date" name="reserve_date" id="birthday"></p>
+                            <p>Birth date
+                                <input type="date" name="reserve_date" id="birthday"></p>
                         </div>
+                        <div class="col-1"></div>
                     </div>
                     <p><input type="submit" name="reserve_submit" class="btn btn-primary"></p>
                 </form>
             </div>
         </div>
     </div>
-</div>
 
 </body>
 </html>
+
+
 
--- View/vaccination_center_panel.php
+++ View/vaccination_center_panel.php
@@ -1,71 +1,101 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Vaccination Center Panel</title>
+    <meta charset="UTF-8">
+    <title>Vaccination center panel</title>
     <link href="Css/bootstrap.min.css" rel="stylesheet">
     <link href="Css/Style_sheet.css" rel="stylesheet">
 </head>
 <body>
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card_header">
-                <h1>VC Manager name</h1>
-            </div>
-        </div>
+<div class="row">
+    <div class="col-12">
+        <div class="cardhead"><h>Welcome</h></div>
     </div>
-    <div class="row">
-        <div class="col-md-6">
-            <div class="card">
-                <p-head class="card-head">Find user</p-head>
-                <div class="card-body">
-                    <form action="" method="post">
-                        <p>Reservation No. : </p>
-                        <p><input  type="text" name="res_no" placeholder="reservation number"></p>
-                        <input type="submit" value="Search" name="res_find" class="btn btn-primary">
-                    </form>
-                </div>
-            </div>
-        </div>
+</div>
 
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-body row">
-                    <form action="" method="post">
-                        <p class="card-text">User's name : </p>
-                        <p class="card-text">National ID :</p>
-                        <p class="card-text">Vaccine name : </p>
-                        <input type="submit" name="res_confirm" value="Confirm" class="btn btn-primary">
-                    </form>
-                </div>
-            </div>
+<div class="row">
+    <div class="col-1"></div>
+    <div class="col-5">
+        <div class="cardbody">
+            <form action="" method="post">
+                <p>Reservation No. : </p>
+                <p><input  type="text" name="res_no" placeholder="reservation number"></p>
+                <input type="submit" value="Search" name="res_find" class="btn btn-primary">
+            </form>
         </div>
     </div>
+    <div class="col-5">
+        <div class="cardbody row">
+            <p>
+            <form action="" method="post">
+                <p class="card-text">User's name : </p>
+                <p class="card-text">National ID :</p>
+                <p class="card-text">Vaccine name : </p>
+                <input type="submit" name="res_confirm" value="Confirm" class="btn btn-primary">
+            </form>
+            </p>
+        </div>
+    </div>
+    <div class="col-1"></div>
+</div>
 
-    <div class="row">
-        <div class="col-md-12">
-            <div class = "card">
-                <table class="table table-bordered table-hover">
-                    <thead>
-                    <tr>
-                        <th scope="col">Reservation No. </th>
-                        <th scope="col">Name</th>
-                        <th scope="col">National ID</th>
-                        <th scope="col">Vaccine name</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>1007</td>
-                        <td>Ahmad</td>
-                        <td>---2102617</td>
-                        <td>dodo</td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
+
+
+
+
+<div class="row">
+    <div class="col">
+        <div class="backcard">
+            <table class="table table-bordered table-hover">
+                <tr>
+                    <th>Reservation number</th>
+
+                    <th>Name</th>
+
+                    <th>National ID</th>
+
+                    <th>Vaccine name</th>
+                </tr>
+
+                <tr>
+                    <td>129483</td>
+
+                    <td>Ahmad</td>
+
+                    <td>20225007</td>
+
+                    <td>corona</td>
+                </tr>
+                <tr>
+                    <td>129483</td>
+
+                    <td>Ahmad</td>
+
+                    <td>20225007</td>
+
+                    <td>corona</td>
+                </tr>
+                <tr>
+                    <td>129483</td>
+
+                    <td>Ahmad</td>
+
+                    <td>20225007</td>
+
+                    <td>corona</td>
+                </tr>
+                <tr>
+                    <td>129483</td>
+
+                    <td>Ahmad</td>
+
+                    <td>20225007</td>
+
+                    <td>corona</td>
+                </tr>
+            </table>
         </div>
     </div>
+</div>
 </body>
 </html>
