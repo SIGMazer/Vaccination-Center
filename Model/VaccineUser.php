65aeb44885057f47f9893adf9274e3c982c65b26
Aliaa33 <aliaaabrahim279@gmail.com>
2022-12-05 21:09:45 +0200

    lastISA

--- Model/VaccineUser.php
+++ Model/VaccineUser.php
@@ -13,13 +13,14 @@ class vaccineuser {
     function __construct() {
 		include_once'../Include/DatabaseClass.php';		
 		$this->db = new database();
-        $sql = "SELECT * FROM vaccineuser,vaccinereservation where $_Session['UserID']";
+        $sql = "SELECT * FROM vaccineuser,vaccinereservation where vaccineuser['UserID']=$_Session['UserID']";
         $row = $this->db->select($sql);
         $this->name=$row['Name'];
         $this->vaccine_ID=$row['VaccineID'];
         $this->reservationDate=$row['Date'];
         $this->doses=$row['DoseNumber'];
-        $this->center_contactNum=$row['Center_ContactNum'];   
+        $this->center_contactNum=$row['Center_ContactNum'];  
+        $this->SecondDoseDate=$row['SecondDoseDate'];
 	}
 
     public function getName(){
@@ -32,26 +33,24 @@ class vaccineuser {
         return $this->reserveStatus;
     }
     public function getReservation(){
-        
-        return $reservationNumber;
+        return $this->reservationNumber;
     }
     public function reserveDose(center_contactNum,vaccine_ID,reservationDate){
-        $sql = "INSERT * FROM vaccinereservation,vaccineuser where vaccineuser['NationalID']= vaccinereservation['User_NationalID']";
+        $sql = "INSERT INTO vaccinereservation($Center_ContactNum,$vaccine_ID) values($this->Center_ContactNum,$this->vaccine_ID),vaccineuser($SecondDoseDate) values($this->SecondDoseDate)";
         $this->center_contactNum = $center_contactNum;
 		$this->vaccine_ID = $vaccine_ID;
         $this->reservationDate = date_create($reservationDate);
         if ($doses==0) {
-            $row = $this->db->insert($sql);
             $reservationNumber="SELECT * FROM vaccinereservation,vaccineuser where vaccineuser['NationalID']= vaccinereservation['User_NationalID']";
             return true;
             $this->db->close();	
         }
         else if($doses==1){
             $row = $this->db->select($sql);
-            $SecondDoseDate= "SELECT SecondDoseDate from vaccineuser";
+            //$SecondDoseDate= "SELECT SecondDoseDate from vaccineuser";
             $diff= date_diff($reservationDate,$SecondDoseDate);
             if($diff->days >0){
-                
+                $row = $this->db->insert($sql);
                 return getReservation();
             }
         }
@@ -61,8 +60,8 @@ class vaccineuser {
     public function listVaccines(){
         $sql = "SELECT * FROM vaccine";
         $vaccineList= $db->display($sql);
-        $vaccineList= $db->check($sql);
-        for($x=0;$x<$vaccineList;$x++){
+        $vaccineNo= $db->check($sql);
+        for($x=0;$x<$vaccineNo;$x++){
             echo "<tr>";
             echo "<td>". $vaccineList[$x]['Name']."</td>";
             echo "<td>". $vaccineList[$x]['Gap']."</td>";
